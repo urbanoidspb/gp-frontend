6b53559e3e7b96014e909252e44dac40848b92ce
vladdlab <vladislavbochmanov@gmail.com>
2019-06-16 16:48:52 +0300

    maybe it'll fix menu

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -160,15 +160,5 @@ export default {
             margin-top: 5vw;
         }
     }
-
-    .fade-enter-active,
-    .fade-leave-active {
-        transition: opacity .3s ease;
-    }
-
-    .fade-enter,
-    .fade-leave-to {
-        opacity: 0;
-    }
 </style>
 
