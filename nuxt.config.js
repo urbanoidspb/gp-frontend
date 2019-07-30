7961d54865ae005c441b95dfe8d3c37f24ec731b
vladdlab <vladislavbochmanov@gmail.com>
2019-07-30 18:44:20 +0300

    adding files

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+      { name: "google-site-verification", content: "Ee2oIsGZ9bDapcGvWcEgLGhhto4NqjBtDzwoAJIc_Lc" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
